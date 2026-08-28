d6cfad69b0e9e74b10fbf9d2f46562901ecb0b38
Muhammad Faizan Farooq <faizanprogrammer12@gmail.com>
2026-08-28 15:05:38 +0500

    Add files via upload

---
+++ overview.py.py
@@ -0,0 +1,87 @@
+#import torch
+#import torch.nn as nn
+
+# for layers we use  
+
+#nn.Linear(3,1)
+#[height,weight,age] = nn.Linear(3,1).parameters() # height and weight are parameters of the linear layer
+
+
+# Neural NetworkModel in Pytorch
+
+# Class Strcture
+# Layers
+# Activation Functions
+# optimiser 
+# Saving Model
+
+# Activation Function
+
+# Rectified Linear Unit (ReLU) Activation Function using  in hidden layers
+# Sigmoid Activation Function  yes/NO using final output
+# softmax used for multipars
+
+
+# Optimiser
+# SGD
+# torch.optm.SGD(model.parameters(),lr =0.01)
+# adm
+# torch.optm.SGD(model.parameters(),lr =0.001)
+
+
+# optimiser.zero_grad()
+# forward pass
+# loss calculation
+# loss.backward()
+# optimiser.step()
+
+# convunotainal network
+# CNNS
+# EDGES
+# CORNERS
+# CURVES
+# TEXTURES
+
+# convutational Neural Network
+
+
+# used for understanding the strcture of image
+
+'''
+in_channel = how many layers in image
+black & white 1
+color RGB 3
+[batch,channel,height,width]
+
+
+output_channel = used for detector pattern
+
+
+kernal size = 3
+3*3 pixels
+kernal size = 5
+5*5
+
+
+stride 
+
+moves filter how many step in one time 
+
+
+
+Pooling 
+
+strong signal ko rakhy gha week ko reject kr dy gha in image 
+ar image sok pahly zoom kry gha a
+'''
+
+
+
+
+
+
+
+
+
+
+    
